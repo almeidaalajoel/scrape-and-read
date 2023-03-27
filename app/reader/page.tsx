7a49953851a0bbb6a9b742a5f156eb11bfe4a48e
almeidaalajoel <30331921+almeidaalajoel@users.noreply.github.com>
2023-03-26 17:47:59 -0700

    look better

--- app/reader/page.tsx
+++ app/reader/page.tsx
@@ -25,8 +25,15 @@ export default async function Reader({
 
     return (
       <Container>
-        <div className="flex flex-grow flex-col w-full lg:w-[65%] xl:w-[53%] text-[rgb(10,10,10)] bg-white dark:bg-[rgb(23,21,21)] dark:text-[rgb(200,200,200)] p-2 lg:p-12 lg:pt-6 leading-7 text-xl border border-solid border-gray-300 dark:border-gray-900">
-          <div className="flex flex-col space-y-4 my-6 break-words text-2xl">
+        <div
+          style={{
+            fontFamily: "Bookerly",
+            lineHeight: "2rem",
+            fontSize: "1.25rem",
+          }}
+          className="flex flex-grow flex-col w-full lg:w-[65%] xl:w-[53%] text-[rgb(10,10,10)] bg-white dark:bg-[rgb(23,21,21)] dark:text-[rgb(200,200,200)] p-2 lg:p-12 lg:pt-6 leading-7 text-xl border border-solid border-gray-300 dark:border-gray-900"
+        >
+          <div className="flex flex-col space-y-8 my-6 break-words">
             {ps.map((ele, i) => {
               if (ele.name == "p") return <p key={i}>{$(ele).text()}</p>;
               else {
