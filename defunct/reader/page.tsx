3d1841b22b10e87c2d17b2736eca3eb03088b7f4
almeidaalajoel <30331921+almeidaalajoel@users.noreply.github.com>
2023-05-20 22:26:46 -0700

    fix import

--- defunct/reader/page.tsx
+++ defunct/reader/page.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import * as cheerio from "cheerio";
-import Navigation from "../components/Navigation";
+import Navigation from "../../app/components/Navigation";
 import { redirect } from "next/navigation";
-import Container from "../components/Container";
+import Container from "../../app/components/Container";
 
 export default async function Reader({
   searchParams: { url },
